d576f9f600f3794ba3b987fc2447707f7c1eea77
curwicka <curwicka@csp.edu>
2021-03-21 16:53:00 -0500

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Page extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  render() {
+    return(
+      <React.Fragment>
+        <header>
+          <h1>Milk Crate</h1>
+          <h3>Buy, Sell, and Share Your Record Collection</h3>
+        </header>
+        <RegistrationForm />
+        <footer>
+          <p>
+            &copy; 2021, MilkCrate:
+            <br />
+            Web Master: <a href="mailto:curwicka@csp.edu">Alexander Curwick</a>
+          </p>
+        </footer>
+      </React.Fragment>
+    );
+  }
+}
+
+class RegistrationForm extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="registrationForm">
+        <h2>New User Registration</h2>
+        <form>
+          <input type='text' id='uname' name='uname' placeholder='Username' /><br />
+          <input type='text' id='fname' name='fname' placeholder='First Name' /><br />
+          <input type='text' id='lname' name='lname' placeholder='Last Name' /><br />
+          <input type='email' id='email' name='email' placeholder='E-Mail' /><br />
+          <input type='password' id='password' name='password' placeholder='Password' /><br />
+          <input type='password' id='confirm' name='confirm' placeholder='Confirm Password' /><br />
+          <input type='button' value='Register' />
+        </form>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(<Page />, document.getElementById('root'));
