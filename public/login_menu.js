a886ef7c7c9289f18c0e6b745d1bcb42ab188df2
curwicka <curwicka@csp.edu>
2021-03-28 10:24:26 -0500

    Failsafe before refactoring

---
+++ public/login_menu.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const e = React.createElement;
+
+class LoginMenu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    <div className="registrationForm">
+      <h2>Login</h2>
+      <form>
+        <input type='text' id='uname' name='uname' placeholder='Username' /><br />
+        <input type='password' id='password' name='password' placeholder='Password' /><br />
+        <input type='button' value='Login' />
+      </form>
+    </div>
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#login');
+ReactDOM.render(e(LoginMenu), domContainer);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './style.css';
 
+{/* Common page components */}
 class Page extends React.Component {
   constructor(props) {
     super(props);
@@ -10,23 +11,14 @@ class Page extends React.Component {
   render() {
     return(
       <React.Fragment>
-        <header>
-          <h1>Milk Crate</h1>
-          <h3>Buy, Sell, and Share Your Record Collection</h3>
-        </header>
-        <RegistrationForm />
-        <footer>
-          <p>
-            &copy; 2021, MilkCrate:
-            <br />
-            Web Master: <a href="mailto:curwicka@csp.edu">Alexander Curwick</a>
-          </p>
-        </footer>
+        <Carousel />
       </React.Fragment>
     );
   }
 }
 
+{/* Login/Register Components */}
+{/*
 class RegistrationForm extends React.Component {
   constructor(props) {
     super(props);
@@ -49,6 +41,95 @@ class RegistrationForm extends React.Component {
     );
   }
 }
+*/}
+class LoginButton extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="login_button">
+        <input type='button' value='Login/Register' />
+      </div>
+    )
+  }
+}
+
+{/* Image Carousel Components */}
+class Carousel extends React.Component {
+	constructor (props) {
+		super(props);
+
+		this.state = {
+			currentImageIndex: 0
+		};
+
+		this.nextSlide = this.nextSlide.bind(this);
+		this.previousSlide = this.previousSlide.bind(this);
+	}
+
+	previousSlide () {
+		const lastIndex = imgUrls.length - 1;
+		const { currentImageIndex } = this.state;
+		const shouldResetIndex = currentImageIndex === 0;
+		const index =  shouldResetIndex ? lastIndex : currentImageIndex - 1;
+
+		this.setState({
+			currentImageIndex: index
+		});
+	}
+
+	nextSlide () {
+		const lastIndex = imgUrls.length - 1;
+		const { currentImageIndex } = this.state;
+		const shouldResetIndex = currentImageIndex === lastIndex;
+		const index =  shouldResetIndex ? 0 : currentImageIndex + 1;
+
+		this.setState({
+			currentImageIndex: index
+		});
+	}
+
+	render () {
+		return (
+			<div className="carousel">
+        <h2>Recently Posted</h2>
+				<Arrow direction="left" clickFunction={ this.previousSlide } glyph="&#9664;" />
+				<ImageSlide url={ imgUrls[this.state.currentImageIndex] } />
+				<Arrow direction="right" clickFunction={ this.nextSlide } glyph="&#9654;" />
+			</div>
+		);
+	}
+}
+
+const ImageSlide = ({ url }) => {
+	const styles = {
+		backgroundImage: `url(${url})`,
+		backgroundSize: 'cover',
+		backgroundPosition: 'center'
+	};
+
+	return (
+		<div className="image-slide" style={styles}></div>
+	);
+}
+
+const Arrow = ({ direction, clickFunction, glyph }) => (
+	<div
+		className={ `slide-arrow ${direction}` }
+		onClick={ clickFunction }>
+		{ glyph }
+	</div>
+);
+
+const imgUrls = [
+	process.env.PUBLIC_URL + '/lp01c.png',
+	process.env.PUBLIC_URL + '/lp02c.png',
+	process.env.PUBLIC_URL + '/lp03c.png',
+	process.env.PUBLIC_URL + '/lp04c.png',
+	process.env.PUBLIC_URL + '/lp05c.png'
+];
 
 // ========================================
 
